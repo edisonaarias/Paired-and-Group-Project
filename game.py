b6ccf9b2c8a84ad4fad7ec43cb7be5b12520ba91
unknown <edisonalexanderarias@gmail.com>
2022-05-16 14:32:06 -0700

    Added classes and added to the games phase

---
+++ game.py
@@ -0,0 +1,15 @@
+from human import Human
+from ai import AI
+
+class Game:
+    def __init__(self):
+        self.player = Player("rock")
+        self.ai = AI("paper")
+
+    def run_game(self):
+
+    def display_welcome(self):
+
+    def rounds(self):
+
+    def display_winner(self):
