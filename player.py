c3ec77dac5130a5d87dff1acf74d7a5a8c70c1cf
unknown <edisonalexanderarias@gmail.com>
2022-05-16 15:05:35 -0700

    Adding code to the player file

---
+++ player.py
@@ -0,0 +1,27 @@
+from http.cookies import SimpleCookie
+from multiprocessing import RLock, parent_process
+from ossaudiodev import SNDCTL_DSP_NONBLOCK
+from typing import ParamSpecArgs
+from human import Human
+
+class Player:
+    def __init__(self, name, players):
+        self.name = name 
+        self.players = players                                                      # need to make sure that self.players can be used to ask user, how many are playing.
+        
+    # we believe hand will have rock,paper scissors,lizard, and spock as parameters.
+
+    def hand(self): 
+        self.rock -= self.attack_power()
+        self.rock = "rock"
+        self.paper = "paper"
+        self.scissors = "scissors"    
+        self.lizard = "lizard" 
+        self.spock = "spock"
+
+    def count_down(self):
+        self.dual = "winner is best two out of three!"
+
+    def selection(self):
+
+    # def gesture selected(self):
