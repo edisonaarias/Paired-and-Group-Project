a5f21eae6ea539ad8adc591454afaf7b80750a4d
unknown <edisonalexanderarias@gmail.com>
2022-05-17 13:21:13 -0700

    adding list to gestures

--- ai.py
+++ ai.py
@@ -1,11 +1,11 @@
-from unicodedata import name
-
+from ai import AI
+from player import Player
 
 class AI:
     def __init__(self, name):
         self.name = name
 
-    def random_selection(self, AI):
+    def random_selection(self):
         pass
 
     #TODO: override the choose_gesture method
--- human.py
+++ human.py
@@ -1,15 +1,32 @@
-<<<<<<< HEAD
+from inspect import getsource
+from site import getuserbase
 from player import Player
 
 class Human:
     def __init__(self):
-        pass
+        self.player_name = ""
 
+    def choose_gesture(self):
+        self.choose_rock = 0
+        self.choose_paper = 1
+        self.choose_scissors = 2
+        self.choose_lizard = 3 
+        self.choose_spock = 4
 
-    #TODO: override the choose_gesture method
-    def hand(self, ai, human):
-        self.
-=======
-class Human:
-    def __init__(self):
->>>>>>> 22a09aa21047493ea902df519368106d8d213199
+    def player_count():
+        self_input = ""
+        # self.surprise = user_input("How many players? 1,2,3")
+
+    a = [0, 1, 2, 3, 4]
+    for x in range(len(a)):
+        print a[x],
+
+# welcome display, 
+# rules displayed along with how to make selections,
+#  print out the meaning of the gestures, 
+# prompt the user to select how many players, 
+# prompt the user to then make an entry to select his gesture,
+# print out which player chose which gesture as well as who won the roudn 
+# print out that the game is moving to the next round
+#  prompt the user to then make an entry to select his gesture 
+# print out whic player chose which gesture as well as who won the round
